6ea0d5a8f869d2e905ae16c3648a143f9abd6b81
4ripley <yud28@ya.ru>
2021-11-23 20:24:54 +0300

    123

--- projects/angular-app-hints/src/lib/angular-app-hints.service.ts
+++ projects/angular-app-hints/src/lib/angular-app-hints.service.ts
@@ -1,14 +1,16 @@
 import { Inject, Injectable } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+export interface ElementStyle {
+  readonly padding?: number;
+  readonly borderRadius?: number;
+}
+
 export interface PathElement {
   readonly selector: string;
   readonly message: string;
   readonly disableClose?: boolean;
-  readonly style?: {
-    readonly padding?: number;
-    readonly borderRadius?: number;
-  }
+  readonly style?: ElementStyle;
 }
 
 export interface AngularAppHintsConfig {
@@ -18,6 +20,8 @@ export interface AngularAppHintsConfig {
   readonly closeOnEscape: boolean;
   readonly enableAnimations: boolean;
   readonly scrollIntoViewOptions: ScrollIntoViewOptions;
+  readonly defaultStyle?: ElementStyle;
+  readonly appendToSelector?: string;
 }
 
 export type Path = PathElement[];
@@ -30,16 +34,16 @@ export class AngularAppHintsService {
   constructor(
     @Inject(DOCUMENT) private document: Document,
   ) {
-    // #region container
-    this.container = this.document.createElement('div');
-    this.container.classList.add('angular-app-hints__container');
-    this.container.classList.add('angular-app-hints__container-animations');
+    // #region pivot
+    this.pivot = this.document.createElement('div');
+    this.pivot.classList.add('angular-app-hints__pivot');
+    this.pivot.classList.add('angular-app-hints__pivot-animations');
     // #endregion
 
     // #region focus
     this.focus = this.document.createElement('div');
     this.focus.classList.add('angular-app-hints__shadow');
-    this.container.appendChild(this.focus);
+    this.pivot.appendChild(this.focus);
 
     this.focusInner = this.document.createElement('div');
     this.focusInner.classList.add('angular-app-hints__shadow-inner');
@@ -54,7 +58,7 @@ export class AngularAppHintsService {
     // #region menu
     this.menu = this.document.createElement('div');
     this.menu.classList.add('angular-app-hints__menu');
-    this.container.appendChild(this.menu);
+    this.pivot.appendChild(this.menu);
 
     const menuInner = this.document.createElement('div');
     menuInner.classList.add('angular-app-hints__menu-inner');
@@ -105,7 +109,8 @@ export class AngularAppHintsService {
   }
 
   private path: Path = [];
-  private container: HTMLElement;
+  private filteredPath: Path = [];
+  private pivot: HTMLElement;
   private focus: HTMLDivElement;
   private focusInner: HTMLDivElement;
   private blockOverlay: HTMLDivElement;
@@ -143,26 +148,34 @@ export class AngularAppHintsService {
     this.prevButton.textContent = this.config.prevText;
     this.nextButton.textContent = this.config.nextText;
     this.closeButton.textContent = this.config.closeText;
-    this.container.classList.toggle(
-      'angular-app-hints__container-animations',
+    this.pivot.classList.toggle(
+      'angular-app-hints__pivot-animations',
       this.config.enableAnimations
     );
   }
 
   public go(): void {
+    this.filteredPath = this.path.filter(
+      i => !!this.document.querySelector(i.selector)
+    );
     if (!this.currentElement) {
-      this.currentElement = this.path[0];
+      this.currentElement = this.filteredPath[0];
       this.setFocused(this.currentElement);
       this.updateMenuContent();
-      this.document.body.appendChild(this.container);
-      this.document.body.appendChild(this.blockOverlay);
+      const appendToSelector = this.config.appendToSelector ?? 'body';
+      const appendTo = this.document.querySelector(appendToSelector);
+      if (!appendTo) {
+        throw new Error(`Element ${appendToSelector} not found`);
+      }
+      appendTo.appendChild(this.pivot);
+      appendTo.appendChild(this.blockOverlay);
     }
   }
 
   private next(): void {
     if (this.currentElement) {
-      const index = this.path.indexOf(this.currentElement) + 1;
-      this.currentElement = this.path[index];
+      const index = this.filteredPath.indexOf(this.currentElement) + 1;
+      this.currentElement = this.filteredPath[index];
       this.setFocused(this.currentElement);
       this.updateMenuContent();
     }
@@ -170,8 +183,8 @@ export class AngularAppHintsService {
 
   private previous(): void {
     if (this.currentElement) {
-      const index = this.path.indexOf(this.currentElement) - 1;
-      this.currentElement = this.path[index];
+      const index = this.filteredPath.indexOf(this.currentElement) - 1;
+      this.currentElement = this.filteredPath[index];
       this.setFocused(this.currentElement);
       this.updateMenuContent();
     }
@@ -179,13 +192,15 @@ export class AngularAppHintsService {
 
   public close(): void {
     if (this.currentElement) {
-      this.document.body.removeChild(this.container);
-      this.document.body.removeChild(this.blockOverlay);
+      const parent = this.pivot.parentElement!;
+      parent.removeChild(this.pivot);
+      parent.removeChild(this.blockOverlay);
       this.currentElement = null;
     }
   }
 
   private setFocused(pathEl: PathElement): void {
+    // TODO: document.scrollingElement.overflow = 'overlay' ?
     if (!this.focus) {
       throw new Error('TODO');
     }
@@ -194,21 +209,28 @@ export class AngularAppHintsService {
       throw new Error('TODO');
     }
     this.message.innerHTML = pathEl.message;
-    const borderRadius = pathEl.style?.borderRadius ?? 0;
-    const padding = pathEl.style?.padding ?? 0;
+    const { borderRadius, padding } = {
+      borderRadius: 0,
+      padding: 0,
+      ...this.config.defaultStyle,
+      ...pathEl.style,
+    };
     const { width, height } = el.getBoundingClientRect();
     this.focusInner.style.borderRadius = borderRadius + 'px';
     this.focusInner.style.padding = padding + 'px';
     this.focusInner.style.margin = -padding + 'px';
     this.focusInner.style.height = height + 'px';
     this.focusInner.style.width = width + 'px';
-    this.setContainerPosition(el);
-    this.scrollIntoElement(el);
-    window.addEventListener('resize', () => this.setContainerPosition(el));
+    this.setPivotPosition(el);
+    this.scrollIntoElement(el); // TODO: menu?
+    window.addEventListener('resize', () => {
+      this.setPivotPosition(el);
+      this.scrollIntoElement(el);
+    });
   }
 
-  private setContainerPosition(el: Element): void {
-    const { top, left, right, bottom } = el.getBoundingClientRect();
+  private setPivotPosition(el: Element): void {
+    const { top, left, right, bottom, width, height } = el.getBoundingClientRect();
 
     const bodyRect = this.document.body.getBoundingClientRect();
     const computedBodyStyle = window.getComputedStyle(this.document.body);
@@ -217,43 +239,57 @@ export class AngularAppHintsService {
     let posX: string;
     let posY: string;
 
-    if (scrollX + left < this.document.body.scrollWidth / 2) {
-      this.container.style.left = `${scrollX + left}px`;
-      posX = 'left';
-    } else {
-      this.container.style.left = `${scrollX + right}px`;
-      posX = 'right';
-    }
-    if (scrollY + top < this.document.body.scrollHeight / 2) {
-      this.container.style.top = `${scrollY + bottom}px`;
-      posY = 'top';
-    } else {
-      this.container.style.top = `${scrollY + top}px`;
-      posY = 'bottom';
-    }
+    const ratio = width > height;
+
+    const topHalf = scrollX + left < this.document.body.scrollWidth / 2;
+    this.pivot.style.left = topHalf === ratio
+      ? `${scrollX + left}px`
+      : `${scrollX + right}px`;
+    posX = topHalf ? 'left' : 'right';
+    const leftHalf = scrollY + top < this.document.body.scrollHeight / 2;
+    this.pivot.style.top = leftHalf === ratio
+      ? `${scrollY + bottom}px`
+      : `${scrollY + top}px`;
+    posY = leftHalf ? 'top' : 'bottom';
     [
       'angular-app-hints__shadow-top-left',
       'angular-app-hints__shadow-top-right',
       'angular-app-hints__shadow-bottom-left',
       'angular-app-hints__shadow-bottom-right',
+      'angular-app-hints__shadow-left-top',
+      'angular-app-hints__shadow-left-bottom',
+      'angular-app-hints__shadow-right-top',
+      'angular-app-hints__shadow-right-bottom',
     ].forEach(i => this.focus.classList.remove(i));
-    this.focus.classList.add(`angular-app-hints__shadow-${posY}-${posX}`);
+    this.focus.classList.add(
+      ratio
+        ? `angular-app-hints__shadow-${posY}-${posX}`
+        : `angular-app-hints__shadow-${posX}-${posY}`
+    );
     [
       'angular-app-hints__menu-top-left',
       'angular-app-hints__menu-top-right',
       'angular-app-hints__menu-bottom-left',
       'angular-app-hints__menu-bottom-right',
+      'angular-app-hints__menu-left-top',
+      'angular-app-hints__menu-left-bottom',
+      'angular-app-hints__menu-right-top',
+      'angular-app-hints__menu-right-bottom',
     ].forEach(i => this.menu.classList.remove(i));
-    this.menu.classList.add(`angular-app-hints__menu-${posY}-${posX}`);
+    this.menu.classList.add(
+      ratio
+        ? `angular-app-hints__menu-${posY}-${posX}`
+        : `angular-app-hints__menu-${posX}-${posY}`
+    );
   }
 
   private updateMenuContent(): void {
     if (!this.currentElement) {
       throw new Error('TODO');
     }
-    const index = this.path.indexOf(this.currentElement);
+    const index = this.filteredPath.indexOf(this.currentElement);
     this.prevButton.style.display = index === 0 ? 'none' : 'block';
-    const last = index === this.path.length - 1;
+    const last = index === this.filteredPath.length - 1;
     this.nextButton.style.display = last ? 'none' : 'block';
     this.closeButton.style.display = !!this.currentElement.disableClose && !last
       ? 'none'
